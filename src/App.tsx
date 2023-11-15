d89423519327fa497ac3140bb7bb2eb5b001fda8
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-11-15 16:42:36 +0200

    Some initial s3 API explorations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,36 @@
+import * as React from 'react';
+import {
+  S3Client,
+  // This command supersedes the ListObjectsCommand and is the recommended way to list objects.
+  ListObjectsCommand,
+} from '@aws-sdk/client-s3';
+
+const client = new S3Client({
+  region: 'eu-north-1',
+  credentials: {
+    accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY,
+  },
+});
+
+export const main = async () => {
+  const command = new ListObjectsCommand({
+    Bucket: 'ionibonboni-test-bucket',
+  });
+
+  try {
+    const celiqObekt = await client.send(command);
+    console.log(celiqObekt);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 function App() {
+  React.useEffect(() => {
+    main();
+  }, []);
+
   return <>HELLO</>;
 }
 
