ab1dbe7617d59adbbd1a971e288fd65db65833d8
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-11-26 11:19:44 +0300

    Figure out object creation, setup test and lucid clients

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,87 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   S3Client,
-  // This command supersedes the ListObjectsCommand and is the recommended way to list objects.
   ListObjectsCommand,
-} from '@aws-sdk/client-s3';
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 
-const client = new S3Client({
-  region: 'eu-north-1',
+const lucidClient = new S3Client({
+  region: import.meta.env.VITE_REGION_LUCID,
   credentials: {
-    accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY,
+    accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID_LUCID,
+    secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY_LUCID,
   },
 });
 
-export const main = async () => {
-  const command = new ListObjectsCommand({
-    Bucket: 'ionibonboni-test-bucket',
+const ioniClient = new S3Client({
+  region: import.meta.env.VITE_REGION_IONI,
+  credentials: {
+    accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID_IONI,
+    secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY_IONI,
+  },
+});
+
+export const listObjectsLucid = async () => {
+  const listObjectsCommand = new ListObjectsCommand({
+    Bucket: import.meta.env.VITE_BUCKET_LUCID,
   });
 
   try {
-    const celiqObekt = await client.send(command);
+    const celiqObekt = await lucidClient.send(listObjectsCommand);
     console.log(celiqObekt);
   } catch (err) {
     console.error(err);
   }
 };
 
+export const listObjectsIoni = async () => {
+  const listObjectsCommand = new ListObjectsCommand({
+    Bucket: import.meta.env.VITE_BUCKET_IONI,
+  });
+
+  try {
+    const celiqObekt = await ioniClient.send(listObjectsCommand);
+    console.log(celiqObekt);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+type CreateObjectInput = {
+  Body: string;
+  Bucket: string;
+  Key: string;
+};
+
+export const createObject = async (
+  input: CreateObjectInput,
+  client: S3Client,
+) => {
+  const createObjectCommand = new PutObjectCommand(input);
+  const response = await client.send(createObjectCommand);
+  console.log(response);
+};
+
 function App() {
   React.useEffect(() => {
-    main();
+    listObjectsLucid();
+    listObjectsIoni();
+    // createObject(
+    //   {
+    //     Body: "test-folder/depth-one-file.go",
+    //     Bucket: import.meta.env.VITE_BUCKET_LUCID,
+    //     Key: "test-folder/depth-one-file.go",
+    //   },
+    //   lucidClient,
+    // );
+    // createObject(
+    //   {
+    //     Body: "test-folder/depth-one-file.go",
+    //     Bucket: import.meta.env.VITE_BUCKET_IONI,
+    //     Key: "test-folder/depth-one-file.go",
+    //   },
+    //   ioniClient,
+    // );
   }, []);
 
   return <>HELLO</>;
