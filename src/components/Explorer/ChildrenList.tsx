47a288be87e3223a4bb9a78bc6cebeb0f5d9e162
Ioan <latrovalievioan@gmail.com>
2023-11-27 17:03:18 +0200

    extract childrenList from Explorer as a separate component

---
+++ src/components/Explorer/ChildrenList.tsx
@@ -0,0 +1,28 @@
+import { ClosedDir } from '@/assets/ClosedDir';
+import { useGetObjectNames } from '@/hooks/useGetObjectNames';
+import { useStore } from '@/store';
+import { getDirectChildren, getDisplayName, isDir } from '@/utils';
+import { FileIcon } from '@/assets/FileIcon';
+
+export const ChildrenList = () => {
+  const objects = useGetObjectNames();
+  const { selectedObject, setSelectedObject } = useStore();
+  const directChildren = getDirectChildren(selectedObject, objects);
+
+  return (
+    <ul>
+      {directChildren
+        .sort((a, b) => Number(isDir(b)) - Number(isDir(a)))
+        .map((c) => (
+          <li
+            key={c}
+            onDoubleClick={() => setSelectedObject(c)}
+            className="explorerItem"
+          >
+            {isDir(c) ? <ClosedDir /> : <FileIcon />}
+            {getDisplayName(c)}
+          </li>
+        ))}
+    </ul>
+  );
+};
--- src/components/Explorer/index.tsx
+++ src/components/Explorer/index.tsx
@@ -1,35 +1,15 @@
-import { ClosedDir } from '@/assets/ClosedDir';
 import './styles.css';
-import { useGetObjectNames } from '@/hooks/useGetObjectNames';
 import { useStore } from '@/store';
-import { getDirectChildren, getDisplayName, isDir } from '@/utils';
-import { FileIcon } from '@/assets/FileIcon';
 import { Breadcrumbs } from './Breadcrumbs';
-import { useGetObject } from '@/hooks/useGetObject';
+import { ChildrenList } from './ChildrenList';
 
 export const Explorer = () => {
-  const objects = useGetObjectNames();
-  const { selectedObject, setSelectedObject } = useStore();
-  const directChildren = getDirectChildren(selectedObject, objects);
-  const objectContent = useGetObject(selectedObject);
+  const { selectedObject } = useStore();
 
   return (
     <div id="explorer">
       <Breadcrumbs />
-      <ul>
-        {directChildren
-          .sort((a, b) => Number(isDir(b)) - Number(isDir(a)))
-          .map((c) => (
-            <li
-              key={c}
-              onDoubleClick={() => setSelectedObject(c)}
-              className="explorerItem"
-            >
-              {isDir(c) ? <ClosedDir /> : <FileIcon />}
-              {getDisplayName(c)}
-            </li>
-          ))}
-      </ul>
+      <ChildrenList />
     </div>
   );
 };
