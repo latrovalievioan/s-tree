7ee74b4f0b04088088c0489c9edf7dd9137b75ac
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-11-27 10:17:42 +0200

    sort by object type in Explorer

--- src/components/Explorer/index.tsx
+++ src/components/Explorer/index.tsx
@@ -1,9 +1,9 @@
-import { ClosedDir } from "@/assets/ClosedDir";
-import "./styles.css";
-import { useGetObjectNames } from "@/hooks/useGetObjectNames";
-import { useStore } from "@/store";
-import { getDirectChildren, getDisplayName, isDir } from "@/utils";
-import { FileIcon } from "@/assets/FileIcon";
+import { ClosedDir } from '@/assets/ClosedDir';
+import './styles.css';
+import { useGetObjectNames } from '@/hooks/useGetObjectNames';
+import { useStore } from '@/store';
+import { getDirectChildren, getDisplayName, isDir } from '@/utils';
+import { FileIcon } from '@/assets/FileIcon';
 
 export const Explorer = () => {
   const objects = useGetObjectNames();
@@ -12,14 +12,16 @@ export const Explorer = () => {
 
   return (
     <div id="explorer">
-      <h1>{selectedDir ? "root/" + selectedDir : "root/"}</h1>
+      <h1>{selectedDir ? 'root/' + selectedDir : 'root/'}</h1>
       <ul>
-        {directChildren.map((c) => (
-          <li className="explorerItem">
-            {isDir(c) ? <ClosedDir /> : <FileIcon />}
-            {getDisplayName(c)}
-          </li>
-        ))}
+        {directChildren
+          .sort((a, b) => Number(isDir(b)) - Number(isDir(a)))
+          .map((c) => (
+            <li key={c} className="explorerItem">
+              {isDir(c) ? <ClosedDir /> : <FileIcon />}
+              {getDisplayName(c)}
+            </li>
+          ))}
       </ul>
     </div>
   );
