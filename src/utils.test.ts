6caa9881671b768ae5e58c83677e5f0a47027fe2
Ioan <latrovalievioan@gmail.com>
2023-12-02 21:45:03 +0200

    isDir tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -5,4 +5,12 @@ describe('isDir', () => {
   it('Shoud return true when the string is empty', () => {
     expect(isDir('')).toBeTruthy();
   });
+
+  it('Shoud return true when the string ends with /', () => {
+    expect(isDir('testing_tests/')).toBeTruthy();
+  });
+
+  it("Shoud return false when the string isn't empty or ends with /", () => {
+    expect(isDir('testing_tests')).toBeFalsy();
+  });
 });
